377dac4901385f969d0b0cbf7ce818b398bb7a4b
Rhine-cats <71894181+Rhine-cats@users.noreply.github.com>
2021-03-29 12:11:34 +0200

    test

---
+++ ProjetPOO/src/Nucleotide.java
@@ -0,0 +1,11 @@
+public class Nucleotide {
+    String lettre;
+    int position;
+    int brinIndex;
+
+    public Nucleotide(String l, int pos, int index) {
+        this.lettre = l;
+        this.position = pos;
+        this.brinIndex = index;
+    }
+}
