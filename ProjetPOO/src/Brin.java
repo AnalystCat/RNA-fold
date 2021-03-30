d0bd6e62f01610199ebdae01d4c2356f6bbbb548
AnalystCat <38720203+AnalystCat@users.noreply.github.com>
2021-03-30 10:40:03 +0200

    Add getdataRfam to brin Delete urlreader file

--- ProjetPOO/src/Brin.java
+++ ProjetPOO/src/Brin.java
@@ -1,52 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
 
 public class Brin {
     String sequence;
-    String dotFormat;
+    String Parentasage;
     int index;
     
-
-    public static String parserStockholmToDot() throws IOException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nom de la famille: ");
-        String str1 = sc.next();
-        //Concatenating the two Strings
-        URL url = new URL("https://rfam.org/family/"+str1+"/alignment/stockholm");
-        /* Ouvre une connection avec l'object URL */
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        //Methode GET
-        connection.setRequestMethod("GET");
-
-        /* Utilise BufferedReader pour lire ligne par ligne */
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        String inputLine;
-
-        StringBuffer content = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-
-        //Ferme BufferedReader
-        in.close();
-        sc.close();
-        System.out.println(url);
-        System.out.println(inputLine);
-
-        return "";
+    public Brin(String sequence, String parentasage, int index) {
+        this.sequence = sequence;
+        this.Parentasage = parentasage;
+        this.index = index;
     }
 
-    public static void main(String[] args) {
+    public static Brin GetDataRfam(int index, String rFamName  ) throws IOException {
+        
+
+    	
+    	
+    	String parentasage = "" ;
+    	String sequence = "";
+    	
+        URL url = new URL("https://rfam.org/family/"+ rFamName +"/alignment/stockholm");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(url.openStream()));
+
+        String inputLine;
+       
+        while ((inputLine = in.readLine()) != null) {
+        	
+        	String  aaa;
+        	if(inputLine.startsWith("#=GC SS_cons")) {
+        		
+        		inputLine = inputLine.substring(13).replaceAll("[<{]", "(").replace("[", "(");
+        		inputLine = inputLine.replaceAll("[>}]", ")").replace("]", ")");
+        		parentasage = inputLine.replaceAll("[;:_]", "-").replaceAll("[ .]","");
+        		
+        	}
+        	if(inputLine.startsWith("#=GC RF")) {
+        		
+        		sequence = inputLine.substring(8).replaceAll("[ .]","").toUpperCase();
+        	}
+        
+        }
+        in.close();
+    	Brin donne = new Brin(sequence, parentasage, index);
+    	
+        return donne;
+    }
+
+
+	public static void main(String[] args) {
         System.out.println("hello");
         try {
-			parserStockholmToDot();
+        	Brin test =GetDataRfam(1,"RF00360");
+        	System.out.println(test.Parentasage);
+        	System.out.println(test.sequence);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
